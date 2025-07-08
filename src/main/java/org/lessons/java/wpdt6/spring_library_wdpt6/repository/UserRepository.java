fd263c2bfb2e5c0402c67f6ac5c6890f258f0635
alessandroschiano98 <schianoski98@gmail.com>
2025-07-08 18:55:17 +0200

    fix: userDetailsService Dao

--- src/main/java/org/lessons/java/wpdt6/spring_library_wdpt6/repository/UserRepository.java
+++ src/main/java/org/lessons/java/wpdt6/spring_library_wdpt6/repository/UserRepository.java
@@ -2,7 +2,6 @@ package org.lessons.java.wpdt6.spring_library_wdpt6.repository;
 
 import org.lessons.java.wpdt6.spring_library_wdpt6.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 
